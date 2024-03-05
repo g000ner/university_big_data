e3aca355bc3e5a288312bb9971c8393300237b09
Dmitry Kiselev <kiselevzfth@gmail.com>
2024-03-05 14:35:45 +0400

    сделал лаб1

--- lab1/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-sudo rm -rf /tmp/maprdemo
-
-sudo mkdir -p /tmp/maprdemo/hive /tmp/maprdemo/zkdata /tmp/maprdemo/pid /tmp/maprdemo/logs /tmp/maprdemo/nfs
-sudo chmod -R 777 /tmp/maprdemo/hive /tmp/maprdemo/zkdata /tmp/maprdemo/pid /tmp/maprdemo/logs /tmp/maprdemo/nfs
-
-docker compose build
